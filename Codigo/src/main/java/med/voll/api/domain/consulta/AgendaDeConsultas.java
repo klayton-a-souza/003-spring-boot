e6589d0945878efb6e6854f92b369782d1cd965d
Klayton Souza <klayton.al.souza@gmail.com>
2023-08-07 07:57:28 -0300

    1.7 - Validações de integridade

--- Codigo/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ Codigo/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,6 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
@@ -20,11 +21,23 @@ public class AgendaDeConsultas {
 
     public void agendar(DadosAgendamentoConsulta dados){
 
-        var medico = medicoRepository.getReferenceById(dados.idMedico());
+        if(!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("Id do paciente informado não existe");
+        }
+
+        if(dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("Id do médico informado não existe");
+        }
+
+        var medico = escolherMedico(dados);
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
 
 
         var consulta = new Consulta(null,medico,paciente,dados.data());
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+
     }
 }
